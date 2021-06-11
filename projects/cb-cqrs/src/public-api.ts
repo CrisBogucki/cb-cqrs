31a4daac798056f4ca4013f6aca0808627f6d351
Krzysztof Bogucki <krzysztof.bogucki@asseco.pl>
2021-06-11 11:05:49 +0200

    fix duplicate identifier 'ɵfac'

--- projects/cb-cqrs/src/public-api.ts
+++ projects/cb-cqrs/src/public-api.ts
@@ -8,21 +8,12 @@ export * from "./lib/cqrs/ServiceBus";
 export * from "./lib/cqrs/command/ICommand";
 export * from "./lib/cqrs/command/IHandleCommand";
 
-//Support compatibility. Better use ServiceBus
-export * from "./lib/cqrs/command/code/CommandBus";
-
 export * from "./lib/cqrs/query/IQuery";
 export * from "./lib/cqrs/query/IResponse";
 export * from "./lib/cqrs/query/IHandleQuery";
 
-//Support compatibility. Better use ServiceBus
-export * from "./lib/cqrs/query/code/QueryBus";
-
 export * from "./lib/cqrs/event/IEvent";
 export * from "./lib/cqrs/event/IHandleEvent";
-
-//Support compatibility. Better use ServiceBus
-export * from "./lib/cqrs/event/code/EventBus";
 
 export * from "./lib/cqrs/base.container";
 
